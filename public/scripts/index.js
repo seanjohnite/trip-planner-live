b07a018722be345f5ad458f2aed732203d97cd5a
Sean Johnston <sean.johnst@gmail.com>
2015-08-20 15:10:56 -0400

    wiring up awesome things

---
+++ public/scripts/index.js
@@ -0,0 +1,19 @@
+// planses
+
+
+// add things to lower panel with add button
+
+// log out the hotel name when + button clicked
+
+$('#item-selector').on('click', 'button', function () {
+  $(this)
+});
+
+
+// remove things from lower panel with x button
+
+// add locations with icons to map and rezoom
+
+// switch between days
+
+// add and remove days with buttons
