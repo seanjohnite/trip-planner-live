8e0552a717c3acd8f29d9eae70a2992713a112e9
Sean Johnston <sean.johnst@gmail.com>
2015-08-20 16:30:14 -0400

    working add to map

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,20 +6,40 @@
 
 // log out the hotel name when + button clicked
 
-// <!-- <div class="itinerary-item">
-//   <span class="title">Bouley</span>
-//   <button class="btn btn-xs btn-danger remove btn-circle">x</button>
-// </div>
+
+function makeListItem (name) {
+  return $('<div class="itinerary-item"><span class="title">' + name + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+}
 
 
 $(document).ready(function() {
   $('#item-selector').on('click', 'button', function () {
     var selection = $(this).prev().children('option').filter(':selected').text();
     var type = $(this).parent().find('h4').text();
-    // var type1 = $(this).prev().prev().text();
+    var location = items[type].filter(function (hotel) {
+      return selection === hotel.name;
+    })[0].place[0].location;
+    console.log(location);
+    var listItem = makeListItem(selection);
+    debugger;
     switch(type) {
-      case 'Hotels':
+      case 'Hotels': 
+        $('#hotel-list').append(listItem); 
+        drawLocation(location, icons.hotel);
+        break;
+      case 'Restaurants': 
+        $('#restaurant-list').append(listItem);
+        drawLocation(location, icons.restaurant); 
+        break;
+      case 'Activities': 
+        $('#activity-list').append(listItem); 
+        drawLocation(location, icons.activity);
+        break;
     }
+  });
+
+  $('#itinerary-panel').on('click', 'button', function () {
+    $(this).parent().remove();
   });
 });
 
